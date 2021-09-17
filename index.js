5d0cfb92a1d83361af5d32d2ef4e453a7709a8b9
astephens-fsd <astephens.pm@icloud.com>
2021-09-17 11:23:03 -0700

    Topic 2, request 3 completed

--- index.js
+++ index.js
@@ -96,10 +96,14 @@ const zooAnimals = [
   Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
   */
 
-  function lowPopulationAnimals(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowPopulationAnimals(zooAnimals){
+    const newArrayPop = zooAnimals.filter(function(item){
+      return item.population < 5;
+    });
+    return newArrayPop;
   }
   
+  console.log('Request 3: .filter', lowPopulationAnimals(zooAnimals));
 
   /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
   The zoo needs to know their total animal population across the United States. 
